8c7ea0720129b9a535b13c201ca87f03af30ef41
Nikita Kar <nikitakar101102@gmail.com>
2026-07-13 15:13:44 +0530

    Make worker/gunicorn concurrency env-tunable for constrained hosting

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Starting Celery worker in background..."
-celery -A celery_app worker --loglevel=info --concurrency=2 &
+celery -A celery_app worker --loglevel=info --concurrency=${CELERY_CONCURRENCY:-2} &
 CELERY_PID=$!
 
 echo "Starting Gunicorn on port ${PORT:-8000}..."
 exec gunicorn config.wsgi:application \
   --bind "0.0.0.0:${PORT:-8000}" \
-  --workers 2 \
+  --workers ${WEB_CONCURRENCY:-2} \
   --timeout 120 \
   --access-logfile - \
   --error-logfile -
